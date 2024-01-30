1ff0987e081defcc451343bcb954e75708b90fc3
GOlukorede <samsongreats@gmail.com>
2024-01-30 13:23:47 -0800

    regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
-
-def match_school(str)
-    # A regular expression that is simply matching School
-    match = str.match(/School/)
-    puts match ? match[0] + "$" : "$"
-end
-
-if ARGV.length == 1
-    match_school(ARGV[0])
-else
-    puts "Usage: #{$PROGRAM_NAME} <string>"
-end
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
